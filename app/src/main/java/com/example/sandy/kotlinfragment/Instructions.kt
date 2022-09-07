36b24f9a9b6e99eb60f2e7f4481f9f96bc6a056e
Tasdid-Hossain <tratul321@gmail.com>
2022-09-07 23:34:15 +0800

    added on translation, edited about.xml, added step on Instructions.kt, added arrow on instructions.xml

--- app/src/main/java/com/example/sandy/kotlinfragment/Instructions.kt
+++ app/src/main/java/com/example/sandy/kotlinfragment/Instructions.kt
@@ -23,32 +23,60 @@ class Instructions:Fragment()
             if(i==0){
                 v.instructions.setText(R.string.take_note)
                 v.takanote.setImageResource(R.drawable.takanote)
+                v.note_right.visibility = View.INVISIBLE
+                v.note_left.visibility = View.INVISIBLE
             }
-            else if (i == 1) {
+            else if(i==1){
                 v.instructions.setText(R.string.second_instruction)
-                v.takanote.setImageResource(R.drawable.mosque)
+                v.note_right.visibility = View.VISIBLE
+                v.note_left.visibility = View.VISIBLE
+                v.takanote.setImageResource(R.drawable.takanote)
+                v.note_right.setImageResource(R.drawable.long_right)
+                v.note_left.setImageResource(R.drawable.long_left)
             }
-
             else if (i == 2) {
                 v.instructions.setText(R.string.third_instruction)
                 v.takanote.setImageResource(R.drawable.phonehold)
+                v.note_right.visibility = View.INVISIBLE
+                v.note_left.visibility = View.INVISIBLE
+            }
+
+            else if (i == 3) {
+                v.instructions.setText("")
+                v.takanote.setImageResource(R.drawable.mosque)
+                v.note_right.visibility = View.INVISIBLE
+                v.note_left.visibility = View.INVISIBLE
             }
         })
 
         v.right_button.setOnClickListener(View.OnClickListener {
-            if (i <3) i++;
+            if (i <4) i++;
             if(i==0){
                 v.instructions.setText(R.string.take_note)
                 v.takanote.setImageResource(R.drawable.takanote)
+                v.note_right.visibility = View.INVISIBLE
+                v.note_left.visibility = View.INVISIBLE
             }
-            else if (i == 1) {
+            else if(i==1){
                 v.instructions.setText(R.string.second_instruction)
-                v.takanote.setImageResource(R.drawable.mosque)
+                v.note_right.visibility = View.VISIBLE
+                v.note_left.visibility = View.VISIBLE
+                v.takanote.setImageResource(R.drawable.takanote)
+                v.note_right.setImageResource(R.drawable.long_right)
+                v.note_left.setImageResource(R.drawable.long_left)
             }
-
             else if (i == 2) {
                 v.instructions.setText(R.string.third_instruction)
                 v.takanote.setImageResource(R.drawable.phonehold)
+                v.note_right.visibility = View.INVISIBLE
+                v.note_left.visibility = View.INVISIBLE
+            }
+
+            else if (i == 3) {
+                v.instructions.setText(R.string.third_instruction)
+                v.takanote.setImageResource(R.drawable.mosque)
+                v.note_right.visibility = View.INVISIBLE
+                v.note_left.visibility = View.INVISIBLE
             }
         })
 
